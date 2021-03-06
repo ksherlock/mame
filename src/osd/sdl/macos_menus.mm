0b5a8865ef2cb01c8add0053be45d33a24a89c6b
Kelvin Sherlock <ksherlock@gmail.com>
2021-03-06 16:45:17 -0500

    add a speed menu.

--- src/osd/sdl/macos_menus.mm
+++ src/osd/sdl/macos_menus.mm
@@ -8,6 +8,9 @@
 
 @interface MenuDelegate : NSObject<NSMenuItemValidation> {
 	running_machine *_machine;
+	NSMenu *_menu;
+
+	ioport_field *_speed;
 }
 
 -(instancetype)initWithMachine: (running_machine *)machine;
@@ -19,6 +22,7 @@
 
 -(void)softReset:(id)sender;
 -(void)hardReset:(id)sender;
+-(void)setSpeed:(id)sender;
 
 @end
 
@@ -47,6 +51,19 @@
 	if (cmd == @selector(toggleFastForward:)) {
 		[menuItem setState: _machine->video().fastforward() ? NSControlStateValueOn : NSControlStateValueOff];
 		return YES;		
+	}
+
+	if (cmd == @selector(setSpeed:)) {
+
+		if (!_speed) {
+			[menuItem setEnabled: NO];
+		} else {
+
+			unsigned tag = [menuItem tag];
+
+			[menuItem setEnabled: YES];
+			[menuItem setState: tag == _speed->live().value ? NSControlStateValueOn : NSControlStateValueOff];
+		}
 	}
 
 	#if 0
@@ -87,105 +104,147 @@
 	_machine->natkeyboard().paste();
 }
 
+-(void)setSpeed:(id)sender {
+
+	if (!_speed) return;
+	unsigned tag = [(NSMenuItem *)sender tag];
+	_speed->live().value = tag;
+}
+
+-(void)buildSpeedMenu {
+
+#if 0
+	auto &list = _machine.ioport().ports();
+	auto iter = list.find(":a2_config");
+	if (iter != list.end()) {
+
+
+
+	}
+#endif
+
+	NSMenu *mainMenu = [NSApp mainMenu];
+
+	for (auto &port : _machine->ioport().ports()) {
+		if (port.first != ":a2_config") continue;
+		for (ioport_field &field : port.second->fields()) {
+			const char *name = field.name();
+			if (!name) continue;
+			if (strcmp("CPU type", name))  continue;
+			_speed = &field;
+
+			NSMenu *menu = [[NSMenu alloc] initWithTitle: @"Speed"];
+
+			for (auto &setting : field.settings()) {
+
+				NSString *title = [NSString stringWithUTF8String: setting.name()];
+				NSMenuItem *item = [menu addItemWithTitle: title action: @selector(setSpeed:) keyEquivalent: @""];
+				[item setTag: setting.value()];
+				[item setTarget: self];
+			}
+
+
+			NSMenuItem *item = [mainMenu addItemWithTitle: @"Speed" action: NULL keyEquivalent: @""];
+			[item setSubmenu: menu];
+			[menu setAutoenablesItems: YES];
+
+			[menu release];
+		}
+
+	}
+}
+
+-(void)buildSpecialMenu {
+
+
+	NSMenu *mainMenu = [NSApp mainMenu];
+
+	_menu = [[NSMenu alloc] initWithTitle: @"Special"];
+
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Pause" action: @selector(togglePause:) keyEquivalent: @"p"];
+		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Throttle" action: @selector(toggleThrottle:) keyEquivalent: @"t"];
+		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Fast Forward" action: @selector(toggleFastForward:) keyEquivalent: @"f"];
+		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"UI Keyboard" action: @selector(toggleKeyboard:) keyEquivalent: @"k"];
+		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [NSMenuItem separatorItem]; [_menu addItem: item];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Soft Reset" action: @selector(softReset:) keyEquivalent: @""];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Hard Reset" action: @selector(hardReset:) keyEquivalent: @""];
+		[item setTarget: self];
+	}
+	{
+		NSMenuItem *item = [NSMenuItem separatorItem]; [_menu addItem: item];
+	}
+	{
+		NSMenuItem *item = [_menu addItemWithTitle: @"Paste Text" action: @selector(paste:) keyEquivalent: @"v"];
+		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
+		[item setTarget: self];
+	}
+
+	[_menu setAutoenablesItems: YES];
+	NSMenuItem *item  = [mainMenu addItemWithTitle: @"Special" action: NULL keyEquivalent: @""];
+	[item setSubmenu: _menu];
+}
+
+-(void)fixMenus {
+
+
+	NSMenu *menu = [NSApp mainMenu];
+	if (!menu) return;
+
+	for (NSMenuItem *a in [menu itemArray]) {
+		for (NSMenuItem *b in [[a submenu] itemArray]) {
+			unsigned m = [b keyEquivalentModifierMask];
+			if (m & NSEventModifierFlagCommand)
+				[b setKeyEquivalentModifierMask: m | NSEventModifierFlagOption];
+
+			/* optional-command-, preferences. */
+			if ([@"," isEqualToString: [b keyEquivalent]]) {
+				[b setTarget: self];
+				[b setAction: @selector(preferences:)];
+			}
+		}
+	}
+
+}
+
 @end
 
 
 
-static NSMenuItem *BuildSpecialMenu(id target){
-	NSMenu *menu = [[NSMenu alloc] initWithTitle: @"Special"];
-	NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Special" action: NULL keyEquivalent: @""];
+void ample_update_machine(running_machine *machine) {
 
-	NSMutableArray *array = [NSMutableArray new];
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Pause" action: @selector(togglePause:) keyEquivalent: @"p"];
-		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Throttle" action: @selector(toggleThrottle:) keyEquivalent: @"t"];
-		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Fast Forward" action: @selector(toggleFastForward:) keyEquivalent: @"f"];
-		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"UI Keyboard" action: @selector(toggleKeyboard:) keyEquivalent: @"k"];
-		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [NSMenuItem separatorItem]; [array addObject: item];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Soft Reset" action: @selector(softReset:) keyEquivalent: @""];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Hard Reset" action: @selector(hardReset:) keyEquivalent: @""];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
-	{
-		NSMenuItem *item = [NSMenuItem separatorItem]; [array addObject: item];
-	}
-	{
-		NSMenuItem *item = [[NSMenuItem alloc] initWithTitle: @"Paste Text" action: @selector(paste:) keyEquivalent: @"v"];
-		[item setKeyEquivalentModifierMask: NSEventModifierFlagOption|NSEventModifierFlagCommand];
-		[item setTarget: target];
-		[array addObject: item];
-		[item release];
-	}
+static MenuDelegate *target = nil;
 
-	[menu setItemArray: array];
-	[menu setAutoenablesItems: YES];
-	[item setSubmenu: menu];
-	[menu release];
-	[item autorelease];
-
-	return item;
-}
-
-
-void sdl_macos_menus(running_machine &machine) {
-
-	static MenuDelegate *target = nil;
-
-	target = [[MenuDelegate alloc] initWithMachine: &machine];
+	target = [[MenuDelegate alloc] initWithMachine: machine];
 
 	@autoreleasepool {
-		if (NSApp) {
-			NSMenu *menu = [NSApp mainMenu];
 
-			for (NSMenuItem *a in [menu itemArray]) {
-				for (NSMenuItem *b in [[a submenu] itemArray]) {
-					unsigned m = [b keyEquivalentModifierMask];
-					if (m & NSEventModifierFlagCommand)
-						[b setKeyEquivalentModifierMask: m | NSEventModifierFlagOption];
-
-					/* optional-command-, preferences. */
-					if ([@"," isEqualToString: [b keyEquivalent]]) {
-						[b setTarget: target];
-						[b setAction: @selector(preferences:)];
-					}
-				}
-			}
-			[menu addItem: BuildSpecialMenu(target)];
-		}
+		[target fixMenus];
+		[target buildSpecialMenu];
+		[target buildSpeedMenu];
 	}
 }
+
 
 #endif
