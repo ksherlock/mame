f0f24c1e1933846123d27448d64b3789f74f781a
Kelvin Sherlock <ksherlock@gmail.com>
2021-03-13 12:23:35 -0500

    adjust keyboard filtering so GS-BUG can still work (at least once control panel interrupts work)

--- src/osd/modules/input/input_sdl.cpp
+++ src/osd/modules/input/input_sdl.cpp
@@ -387,7 +387,8 @@ public:
 
 			#ifdef AMPLE
 			/* ignore option-command-alpha menu items */
-			if (keyboard.state[SDL_SCANCODE_LALT] && keyboard.state[SDL_SCANCODE_LGUI] && sdlevent.key.keysym.scancode < SDL_SCANCODE_CAPSLOCK)
+			/* GSBug uses control-option-command-esc  */
+			if (keyboard.state[SDL_SCANCODE_LALT] && keyboard.state[SDL_SCANCODE_LGUI] && sdlevent.key.keysym.scancode < SDL_SCANCODE_ESCAPE)
 				break;
 			#endif
 			keyboard.state[sdlevent.key.keysym.scancode] = 0x80;
