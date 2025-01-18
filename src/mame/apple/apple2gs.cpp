b0a6852f8724ee9f6c080e69c72f6ec220f76a9e
Kelvin Sherlock <ksherlock@gmail.com>
2025-01-17 22:26:07 -0500

    ignore apple iigs bad rom complaints.

--- src/mame/apple/apple2gs.cpp
+++ src/mame/apple/apple2gs.cpp
@@ -3958,6 +3958,11 @@ void apple2gs_state::apple2gsmt(machine_config &config)
 	applefdintf_device::add_35_hd(config, m_floppy[3]);
 }
 
+#ifdef AMPLE
+// don't complain about character rom.
+#undef BAD_DUMP
+#define BAD_DUMP ""
+#endif
 /***************************************************************************
 
   Game driver(s)
