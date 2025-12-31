ca62d1c7d761300fe561f98cf15c5c836809008a
Kelvin Sherlock <ksherlock@gmail.com>
2025-12-30 20:37:19 -0500

    Add A2retroNET card, this time implemented in terms of the SSC card.

--- src/devices/bus/a2bus/a2ssc.cpp
+++ src/devices/bus/a2bus/a2ssc.cpp
@@ -19,6 +19,16 @@
 #include "bus/rs232/rs232.h"
 #include "machine/mos6551.h"
 
+#include "imagedev/harddriv.h"
+#include "multibyte.h"
+
+
+#define LOG_CMD      (1U << 1)
+
+#define VERBOSE (0)
+
+// #define LOG_OUTPUT_FUNC osd_printf_info
+#include "logmacro.h"
 
 namespace {
 
@@ -81,6 +91,130 @@ private:
 	bool m_alt_bank;
 };
 
+
+
+class a2bus_retronet_device: public a2bus_ssc_device
+{
+public:
+	a2bus_retronet_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock);
+
+protected:
+	// construction/destruction
+	a2bus_retronet_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock);
+
+	virtual void device_start() override ATTR_COLD;
+	// virtual void device_reset() override ATTR_COLD;
+	virtual void device_add_mconfig(machine_config &config) override ATTR_COLD;
+	virtual const tiny_rom_entry *device_rom_region() const override ATTR_COLD;
+	// virtual ioport_constructor device_input_ports() const override ATTR_COLD;
+
+	// overrides of standard a2bus slot functions
+	// virtual uint8_t read_c0nx(uint8_t offset) override;
+	// virtual void write_c0nx(uint8_t offset, uint8_t data) override;
+	virtual uint8_t read_cnxx(uint8_t offset) override;
+	virtual uint8_t read_c800(uint16_t offset) override;
+	virtual void write_c800(uint16_t offset, uint8_t data) override;
+	virtual bool take_c800() const override { return true; }
+	virtual void reset_from_bus() override;
+
+private:
+
+	uint8_t read_cffx(uint8_t offset);
+	void write_cffx(uint8_t offset, uint8_t data);
+
+	void do_control();
+
+	uint8_t unit_to_drive(uint8_t unit) const;
+
+
+	uint8_t hdd_read(uint8_t drive, uint32_t block, uint8_t *data);
+	uint8_t hdd_write(uint8_t drive, uint32_t block, const uint8_t *data);
+	size_t hdd_blocks(uint8_t drive);
+
+	uint8_t pro_stat(void);
+	uint8_t pro_read(void);
+	uint8_t pro_write(void);
+
+	uint8_t sp_stat(void);
+	uint8_t sp_read(void);
+	uint8_t sp_write(void);
+
+	// required_region_ptr<uint8_t> m_rom;
+	required_device_array<harddisk_image_device, 8> m_drive;
+
+
+	std::error_condition load_hd(device_image_interface &image) const;
+
+
+	uint8_t m_sp_control = 0;
+	uint8_t m_output_mask = 0;
+	uint16_t m_sp_read_offset = 0;
+	uint16_t m_sp_write_offset = 0;
+	uint16_t m_offset = 0;
+
+	uint8_t m_sp_buffer[1024];
+
+	};
+
+#define IOSEL_OFFSET 0x1000
+#define IOSTRB_OFFSET 0x2000
+
+#define CONTROL_NONE    0x00
+#define CONTROL_PRODOS  0x01
+#define CONTROL_SP      0x02
+#define CONTROL_DONE    0x80
+
+#define PRODOS_CMD_STATUS   0x00
+#define PRODOS_CMD_READ     0x01
+#define PRODOS_CMD_WRITE    0x02
+
+#define PRODOS_I_CMD    0
+#define PRODOS_I_UNIT   1
+#define PRODOS_I_BLOCK  2
+#define PRODOS_I_BUFFER 4
+
+#define PRODOS_O_RETVAL 0
+#define PRODOS_O_BUFFER 1
+
+#define SP_CMD_STATUS   0x00
+#define SP_CMD_READBLK  0x01
+#define SP_CMD_WRITEBLK 0x02
+#define SP_CMD_FORMAT   0x03
+#define SP_CMD_CONTROL  0x04
+#define SP_CMD_INIT     0x05
+#define SP_CMD_OPEN     0x06
+#define SP_CMD_CLOSE    0x07
+#define SP_CMD_READ     0x08
+#define SP_CMD_WRITE    0x09
+
+#define SP_I_CMD    0
+#define SP_I_PARAMS 2
+#define SP_I_BUFFER 10
+
+#define SP_O_RETVAL 0
+#define SP_O_BUFFER 1
+
+#define SP_PARAM_UNIT   0
+#define SP_PARAM_CODE   3
+#define SP_PARAM_BLOCK  3
+
+#define SP_STATUS_STS   0x00
+#define SP_STATUS_DCB   0x01
+#define SP_STATUS_NLS   0x02
+#define SP_STATUS_DIB   0x03
+
+#define SP_SUCCESS  0x00
+#define SP_BADCMD   0x01
+#define SP_BUSERR   0x06
+#define SP_BADCTL   0x21
+
+#define SUCCESS     0x00
+#define IO_ERROR    0x27
+#define WRITE_PROT  0x2B
+
+
+
+
 /***************************************************************************
     PARAMETERS
 ***************************************************************************/
@@ -93,6 +227,12 @@ ROM_END
 ROM_START( ssi )
 	ROM_REGION(0x2000, "program", 0) // "SSI Version 1.1"
 	ROM_LOAD( "apricorn super serial imager - rom.bin", 0x0000, 0x2000, CRC(d251f7f1) SHA1(19f2bc1e60c3fd5e179c4a38f7ca4e3221553562) )
+ROM_END
+
+
+ROM_START( a2retronet ) // 2025-07-10
+	ROM_REGION(0x4000, "program", 0)
+	ROM_LOAD( "a2retronet-2025-07-08.bin", 0x0000, 0x4000, CRC(e34beaed) SHA1(e5dfa4ace24670997d4b3c2834c64e0c3af0b3ab))
 ROM_END
 
 static INPUT_PORTS_START( ssc )
@@ -265,6 +405,13 @@ ioport_constructor apricorn_ssi_device::device_input_ports() const
 	return INPUT_PORTS_NAME( ssi );
 }
 
+#if 0
+ioport_constructor a2bus_retronet_device::device_input_ports() const
+{
+	return nullptr;
+}
+#endif
+
 //-------------------------------------------------
 //  device_add_mconfig - add device configuration
 //-------------------------------------------------
@@ -285,6 +432,32 @@ void a2bus_ssc_device::device_add_mconfig(machine_config &config)
 	rs232.cts_handler().set(m_acia, FUNC(mos6551_device::write_cts));
 }
 
+void a2bus_retronet_device::device_add_mconfig(machine_config &config) {
+	a2bus_ssc_device::device_add_mconfig(config);
+
+	for (unsigned i = 0; i < m_drive.size(); ++i) {
+		HARDDISK(config, m_drive[i], 0);
+		m_drive[i]->set_device_load(FUNC(a2bus_retronet_device::load_hd));
+	}
+}
+
+/*
+ * for now, disk images need to have 512-byte sectors.  
+ */
+std::error_condition a2bus_retronet_device::load_hd(device_image_interface &image) const
+{
+
+	harddisk_image_device *disk = downcast<harddisk_image_device *>(&image);
+	if (!disk->exists())
+		return image_error::UNSPECIFIED;
+
+	if (disk->get_info().sectorbytes != 512)
+		return image_error::INVALIDIMAGE;
+
+	return std::error_condition();
+}
+
+
 //-------------------------------------------------
 //  rom_region - device-specific ROM region
 //-------------------------------------------------
@@ -298,6 +471,12 @@ const tiny_rom_entry *apricorn_ssi_device::device_rom_region() const
 {
 	return ROM_NAME( ssi );
 }
+
+const tiny_rom_entry *a2bus_retronet_device::device_rom_region() const
+{
+	return ROM_NAME( a2retronet );
+}
+
 
 //**************************************************************************
 //  LIVE DEVICE
@@ -325,6 +504,17 @@ apricorn_ssi_device::apricorn_ssi_device(const machine_config &mconfig, const ch
 {
 }
 
+a2bus_retronet_device::a2bus_retronet_device(const machine_config &mconfig, device_type type, const char *tag, device_t *owner, uint32_t clock) :
+	a2bus_ssc_device(mconfig, type, tag, owner, clock),
+	m_drive(*this, "hdd%u", 0)
+{
+}
+
+a2bus_retronet_device::a2bus_retronet_device(const machine_config &mconfig, const char *tag, device_t *owner, uint32_t clock) :
+		a2bus_retronet_device(mconfig, A2BUS_A2RETRONET, tag, owner, clock)
+{
+}
+
 //-------------------------------------------------
 //  device_start - device-specific startup
 //-------------------------------------------------
@@ -338,14 +528,37 @@ void apricorn_ssi_device::device_start()
 	save_item(NAME(m_alt_bank));
 }
 
+void a2bus_retronet_device::device_start()
+{
+	save_item(NAME(m_sp_control));
+	save_item(NAME(m_output_mask));
+	save_item(NAME(m_sp_read_offset));
+	save_item(NAME(m_sp_write_offset));
+	save_item(NAME(m_offset));
+	save_item(NAME(m_sp_buffer));
+}
+
 void a2bus_ssc_device::device_reset()
 {
 }
+
 
 void a2bus_ssc_device::reset_from_bus()
 {
 	m_acia->reset();
 }
+
+void a2bus_retronet_device::reset_from_bus()
+{
+	a2bus_ssc_device::reset_from_bus();
+
+	m_sp_control = CONTROL_NONE;
+	m_output_mask = 0b11111111;
+	m_sp_read_offset = 0;
+	m_sp_write_offset = 0;
+	m_offset = 0;
+}
+
 
 /*-------------------------------------------------
     read_cnxx - called for reads from this card's cnxx space
@@ -362,6 +575,12 @@ uint8_t apricorn_ssi_device::read_cnxx(uint8_t offset)
 		m_alt_bank = false;
 	return m_rom[offset | (BIT(m_dsw1->read(), 8) ? 0x1700 : 0x0700)];
 }
+
+uint8_t a2bus_retronet_device::read_cnxx(uint8_t offset) {
+
+	return m_rom[m_offset | (slotno() << 8) | offset];
+}
+
 
 void apricorn_ssi_device::write_cnxx(uint8_t offset, uint8_t data)
 {
@@ -386,6 +605,20 @@ uint8_t apricorn_ssi_device::read_c800(uint16_t offset)
 		offset |= 0x800;
 	return m_rom[offset];
 }
+
+uint8_t a2bus_retronet_device::read_c800(uint16_t offset) {
+
+	if (offset >= 0x7f0) return read_cffx(offset & 0x0f);
+
+	return m_rom[m_offset | 0x0800 | offset];
+}
+
+void a2bus_retronet_device::write_c800(uint16_t offset, uint8_t data) {
+	if (offset >= 0x7f0) return write_cffx(offset & 0x0f, data);
+}
+
+
+
 
 /*-------------------------------------------------
     read_c0nx - called for reads from this card's c0nx space
@@ -444,6 +677,359 @@ void a2bus_ssc_device::acia_irq_w(int state)
 	}
 }
 
+uint8_t a2bus_retronet_device::read_cffx(uint8_t offset) {
+
+	// printf("read_cffx(%02x)\n", offset);
+
+	uint8_t rv = -1;
+
+	switch (offset & 0x0f) {
+		case 0:
+			rv = m_sp_buffer[m_sp_read_offset];
+			if (!machine().side_effects_disabled())
+				m_sp_read_offset++;
+			break;
+		case 1:
+			rv = m_sp_control;
+			break;
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			break;
+		case 9:
+			if (!machine().side_effects_disabled())
+			 	m_output_mask = 0b01111111;
+			break;
+		case 10:
+			if (!machine().side_effects_disabled())
+				m_output_mask = 0b11111111;
+			break;
+		case 11:
+			if (!machine().side_effects_disabled())
+				m_offset &= ~IOSTRB_OFFSET;
+			break;
+		case 12:
+			if (!machine().side_effects_disabled())
+				m_offset |= IOSTRB_OFFSET;
+			break;
+		case 13:
+			if (!machine().side_effects_disabled())
+				m_offset &= ~IOSEL_OFFSET;
+			break;
+		case 14:
+			if (!machine().side_effects_disabled())
+				m_offset |= IOSEL_OFFSET;
+			break;
+
+		case 15:
+			// m_active = false;
+			break;
+
+	}
+
+	return rv;
+}
+
+void a2bus_retronet_device::write_cffx(uint8_t offset, uint8_t data) {
+
+	// fprintf(stderr, "write_cffx(%02x, %02x)\n", offset, data);
+
+	switch (offset & 0x0f) {
+		case 0:
+			m_sp_buffer[m_sp_write_offset++] = data;
+			break;
+		case 1:
+			m_sp_control = data;
+			do_control();
+			break;
+
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+		case 7:
+		case 8:
+			break;
+
+		case 9:
+			 m_output_mask = 0b01111111;
+			break;
+		case 10:
+			m_output_mask = 0b11111111;
+			break;
+		case 11:
+			m_offset &= ~IOSTRB_OFFSET;
+			break;
+		case 12:
+			m_offset |= IOSTRB_OFFSET;
+			break;
+		case 13:
+			m_offset &= ~IOSEL_OFFSET;
+			break;
+		case 14:
+			m_offset |= IOSEL_OFFSET;
+			break;
+
+		case 15:
+			// m_active = false;
+			break;
+	}
+}
+
+
+
+void a2bus_retronet_device::do_control() {
+
+	unsigned command;
+	[[maybe_unused]] unsigned unit;
+	unsigned rv;
+
+	switch (m_sp_control) {
+	case CONTROL_PRODOS:
+
+		command = m_sp_buffer[PRODOS_I_CMD];
+		unit = m_sp_buffer[PRODOS_I_UNIT];
+		rv = IO_ERROR;
+
+		switch (command) {
+		case PRODOS_CMD_STATUS:
+			LOGMASKED(LOG_CMD, "PD CmdStatus(Device=$%02X)", unit);
+			rv = pro_stat();
+            break;
+
+		case PRODOS_CMD_READ:
+			LOGMASKED(LOG_CMD, "PD CmdRead(Device=$%02X)", unit);
+          	rv = pro_read();
+            break;
+
+		case PRODOS_CMD_WRITE:
+			LOGMASKED(LOG_CMD, "PD CmdWrite(Device=$%02X)", unit);
+            rv = pro_write();
+            break;
+		}
+		m_sp_buffer[PRODOS_O_RETVAL] = rv;
+		break;
+
+	case CONTROL_SP:
+
+		command = m_sp_buffer[SP_I_CMD];
+		unit = m_sp_buffer[SP_I_PARAMS + SP_PARAM_UNIT];
+
+		rv = SP_BADCMD;
+        switch (command) {
+            case SP_CMD_STATUS:
+				LOGMASKED(LOG_CMD, "SP CmdStatus(Device=$%02X)", unit);
+                rv = sp_stat();
+                break;
+            case SP_CMD_READBLK:
+				LOGMASKED(LOG_CMD, "SP CmdReadBlock(Device=$%02X)", unit);
+				rv = sp_read();
+                break;
+            case SP_CMD_WRITEBLK:
+				LOGMASKED(LOG_CMD, "SP CmdWriteBlock(Device=$%02X)", unit);
+				rv = sp_write();
+                break;
+            case SP_CMD_FORMAT:
+                LOGMASKED(LOG_CMD, "SP CmdFormat(Device=$%02X)", unit);
+                break;
+            case SP_CMD_CONTROL:
+                LOGMASKED(LOG_CMD, "SP CmdControl(Device=$%02X)", unit);
+                break;
+            case SP_CMD_INIT:
+                LOGMASKED(LOG_CMD, "SP CmdInit(Device=$%02X)", unit);
+                rv = SP_SUCCESS;
+                break;
+            case SP_CMD_OPEN:
+                LOGMASKED(LOG_CMD, "SP CmdOpen(Device=$%02X)", unit);
+                break;
+            case SP_CMD_CLOSE:
+                LOGMASKED(LOG_CMD, "SP CmdClose(Device=$%02X)", unit);
+                break;
+            case SP_CMD_READ:
+                LOGMASKED(LOG_CMD, "SP CmdRead(Device=$%02X)", unit);
+                break;
+            case SP_CMD_WRITE:
+                LOGMASKED(LOG_CMD, "SP CmdWrite(Device=$%02X)", unit);
+                break;
+        }
+        m_sp_buffer[SP_O_RETVAL] = rv;
+		break;
+	}
+
+	m_sp_read_offset = 0;
+	m_sp_write_offset = 0;
+	m_sp_control = CONTROL_DONE;
+}
+
+
+
+uint8_t a2bus_retronet_device::hdd_read(uint8_t drive, uint32_t block, uint8_t *data) {
+	if (drive >= m_drive.size()) return IO_ERROR;
+
+	harddisk_image_device *disk = m_drive[drive];
+
+	if (disk && disk->exists()) {
+		// TODO -- handle CHD with > 512 blocks?
+		if (disk->read(block, data)) return SUCCESS;
+	}
+
+	return IO_ERROR;
+}
+
+uint8_t a2bus_retronet_device::hdd_write(uint8_t drive, uint32_t block, const uint8_t *data) {
+	if (drive >= m_drive.size()) return IO_ERROR;
+
+	harddisk_image_device *disk = m_drive[drive];
+	if (disk && disk->exists()) {
+		// TODO -- handle CHD with > 512 blocks?
+		if (disk->write(block, data)) return SUCCESS;
+	}
+
+	return IO_ERROR;
+
+
+}
+
+
+size_t a2bus_retronet_device::hdd_blocks(uint8_t drive) {
+	if (drive >= m_drive.size()) return 0;
+
+	harddisk_image_device *disk = m_drive[drive];
+	if (disk && disk->exists()) {
+		const hard_disk_file::info &info = disk->get_info();
+		size_t blocks = (info.cylinders * info.heads * info.sectors * info.sectorbytes ) / 512;
+
+		// printf("cylinders: %ld heads: %ld sectors: %ld sectorbytes: %ld blocks: %ld\n",
+			// (long)info.cylinders, (long)info.heads, (long)info.sectors, (long)info.sectorbytes, (long)blocks);
+		return blocks;
+	}
+	return 0;
+}
+
+
+
+uint8_t a2bus_retronet_device::unit_to_drive(uint8_t unit) const {
+    uint8_t drive = unit >> 7;
+    if ((unit >> 4 & 0x07) != slotno()) {
+        drive += 0x02;
+    } 
+    return drive;
+}
+
+
+uint8_t a2bus_retronet_device::pro_stat(void) {
+	const uint8_t unit = m_sp_buffer[PRODOS_I_UNIT];
+
+	// printf("pro_stat(%02x (%02x))\n", unit, unit_to_drive(unit));
+
+	size_t blocks = hdd_blocks(unit_to_drive(unit));
+	put_u16le(&m_sp_buffer[PRODOS_O_BUFFER], blocks);
+	return blocks ? SUCCESS : IO_ERROR;
+}
+
+
+uint8_t a2bus_retronet_device::pro_read(void) {
+	const uint8_t unit = m_sp_buffer[PRODOS_I_UNIT];
+	const uint16_t block = get_u16le(&m_sp_buffer[PRODOS_I_BLOCK]);
+
+	// printf("pro_read(%02x (%02x), %04x)\n", unit, unit_to_drive(unit), block);
+
+    return hdd_read(unit_to_drive(unit), block, &m_sp_buffer[PRODOS_O_BUFFER]);	
+}
+
+uint8_t a2bus_retronet_device::pro_write(void) {
+	const uint8_t unit = m_sp_buffer[PRODOS_I_UNIT];
+	const uint16_t block = get_u16le(&m_sp_buffer[PRODOS_I_BLOCK]);
+
+	// printf("pro_write(%02x (%02x), %04x)\n", unit, unit_to_drive(unit), block);
+
+    return hdd_write(unit_to_drive(unit), block, &m_sp_buffer[PRODOS_I_BUFFER]);	
+}
+
+
+uint8_t a2bus_retronet_device::sp_stat(void) {
+	const uint8_t unit = m_sp_buffer[SP_I_PARAMS + SP_PARAM_UNIT];
+	const uint8_t code = m_sp_buffer[SP_I_PARAMS + SP_PARAM_CODE];
+	uint8_t *stat_list = &m_sp_buffer[SP_O_BUFFER];
+
+	// fprintf(stderr, "sp_stat %02x %02x\n", unit, code);
+
+	if (unit == 0) {
+		if (code == SP_STATUS_STS) {
+			memset(stat_list, 0x00, 8);
+
+			unsigned count = 0;
+			for (unsigned i = 0; i < m_drive.size(); ++i) {
+				if (m_drive[i] && m_drive[i]->exists()) count++;
+			}
+
+			put_u16le(&stat_list[0], 8); // size
+			stat_list[2 + 0] = count; /* number of drives */
+			stat_list[2 + 1] = 0b01000000; // // block, write, read, online
+    		return SP_SUCCESS;
+		}
+	} else {
+		if (code == SP_STATUS_STS || code == SP_STATUS_DIB) {
+			const bool status = code == SP_STATUS_STS;
+
+			memset(stat_list, 0x00, 26);
+
+			size_t blocks = hdd_blocks(unit - 1);
+			if (blocks) {
+				stat_list[2 + 0] = 0b11110000;  // block, write, read, online
+			} else {
+				stat_list[2 + 0] = 0b11100000;  // block, write, read;
+			}
+			put_u24le(&stat_list[2+1], blocks);
+
+            if (status) {
+				put_u16le(&stat_list[0], 4); // size
+            } else {
+				put_u16le(&stat_list[0], 25); // size
+
+                stat_list[2 +  4] = 10;   // id string length
+                memcpy(&stat_list[2 + 5], "A2RETRONET      ", 16);
+                stat_list[2 + 21] = 0x02;   // hard disk
+                stat_list[2 + 22] = 0x00;   // removable
+				put_u16le(&stat_list[2 + 23], 0x01); // firmware version
+            }
+            return SP_SUCCESS;
+		}
+	}
+
+	return SP_BADCTL;
+}
+
+uint8_t a2bus_retronet_device::sp_read() {
+	const uint8_t *params = &m_sp_buffer[SP_I_PARAMS];
+	uint8_t *buffer = &m_sp_buffer[SP_O_BUFFER];
+
+	const uint8_t unit = params[SP_PARAM_UNIT];
+	const uint32_t block = get_u24le(&params[SP_PARAM_BLOCK]);
+
+	// fprintf(stderr, "sp_read %02x %06x\n", unit, block);
+
+	return hdd_read(unit - 1, block, buffer);
+}
+
+uint8_t a2bus_retronet_device::sp_write() {
+	const uint8_t *params = &m_sp_buffer[SP_I_PARAMS];
+	const uint8_t *buffer = &m_sp_buffer[SP_I_BUFFER];
+
+	const uint8_t unit = params[SP_PARAM_UNIT];
+	const uint32_t block = get_u24le(&params[SP_PARAM_BLOCK]);
+
+	// fprintf(stderr, "sp_write %02x %06x\n", unit, block);
+
+	return hdd_write(unit - 1, block, buffer);
+}
+
+
 } // anonymous namespace
 
 
@@ -453,3 +1039,4 @@ void a2bus_ssc_device::acia_irq_w(int state)
 
 DEFINE_DEVICE_TYPE_PRIVATE(A2BUS_SSC,    device_a2bus_card_interface, a2bus_ssc_device,    "a2ssc",   "Apple Super Serial Card")
 DEFINE_DEVICE_TYPE_PRIVATE(APRICORN_SSI, device_a2bus_card_interface, apricorn_ssi_device, "aprissi", "Apricorn Super Serial Imager")
+DEFINE_DEVICE_TYPE_PRIVATE(A2BUS_A2RETRONET, device_a2bus_card_interface, a2bus_retronet_device, "a2retronet", "A2retroNET SmartPort Card")
